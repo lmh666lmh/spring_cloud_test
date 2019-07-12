1fa08298c572fa78494990ea849ad0d9e860f05c
lmh666lmh <670007629@qq.com>
2019-07-12 15:18:09 +0800

    消息总线

--- config-client/src/main/java/test/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/test/configclient/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package test.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,9 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @ComponentScan
+@EnableDiscoveryClient
+@RestController
+@RefreshScope
 public class ConfigClientApplication {
+
+	/**
+	 * http://localhost:8881/actuator/bus-refresh   使用postman发送POST请求
+	 * /actuator/bus-refresh接口可以指定服务，即使用"destination"参数，
+	 * 比如 “/actuator/bus-refresh?destination=customers:**” 即刷新服务名为customers的所有服务。
+	 *
+	 * 当git文件更改的时候，通过pc端用post 向端口为8882的config-client发送请求/bus/refresh／；
+	 * 此时8882端口会发送一个消息，由消息总线向其他服务传递，从而使整个微服务集群都达到更新配置文件。
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
+
+	@Value("${foo}")
+	String foo;
+
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+
 }
--- config-server/src/main/java/test/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/test/configserver/ConfigServerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+//第六篇: 分布式配置中心(Spring Cloud Config)(Finchley版本)
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer   //@EnableConfigServer 注解 开启配置服务器的功能
